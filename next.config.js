35484ce7099bf0cacd1a850a4db28b436e9cb61d
Ismail Ilhomov <ismoil6ek@gmail.com>
2024-09-19 14:20:22 +0500

    refactor: pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-});
+const withPWA = require("next-pwa");
 
 module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  },
   reactStrictMode: true,
 });
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,49 @@
-// service-worker.js (for basic setup)
+// public/service-worker.js
+
+const CACHE_NAME = "offline-cache";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/marker_green_icon.png",
+  "/marker_red_icon.png",
+  "/_next/static/*", // Cache Next.js static files
+  // Add any other paths (CSS, JS, map tiles) that you need to cache
+];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("offline-cache").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/marker_green_icon.png",
-        "/marker_red_icon.png",
-        "/path/to/other/resources",
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
+    caches
+      .match(event.request)
+      .then((response) => {
+        // Serve the cached response if available
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        // If both the network and cache fail, return an offline page or resource
+        return caches.match("/offline.html");
+      })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
