6756e09e5a658b719ff5b4fcb998a9cea4864fb3
Ismail Ilhomov <ismoil6ek@gmail.com>
2024-09-19 14:51:06 +0500

    meta: viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
