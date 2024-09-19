866073e11cfee214bb48421df7af15ab976ca0c0
Ismail Ilhomov <ismoil6ek@gmail.com>
2024-09-19 15:24:49 +0500

    refactor: pwa and offline worker

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
-// next.config.js
-
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
   dest: "public",
-  disable: process.env.NODE_ENV === "development", // Enable in production
-  register: true,
-  skipWaiting: true,
+  fallbacks: {
+    //image: "/static/images/fallback.png",
+    document: "/offline", // if you want to fallback to a custom page rather than /_offline
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
 });
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... other options you like
+};
 
-module.exports = withPWA({
-  reactStrictMode: true,
-});
+module.exports = withPWA(nextConfig);
